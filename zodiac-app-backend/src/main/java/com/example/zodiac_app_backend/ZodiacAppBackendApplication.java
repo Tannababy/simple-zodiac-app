4e39bc04d5a66d818f4c8cb0d3ce00612c7e3927
Lotanna <tannaloamobi@gmail.com>
2025-08-20 19:53:38 +0100

    created springboot backend

---
+++ zodiac-app-backend/src/main/java/com/example/zodiac_app_backend/ZodiacAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.zodiac_app_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZodiacAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZodiacAppBackendApplication.class, args);
+	}
+
+}
---
+++ zodiac-app-backend/src/test/java/com/example/zodiac_app_backend/ZodiacAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zodiac_app_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZodiacAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
